29003ca6caa29c4c5d9b95dfa60d63263c27d78f
destroyme123 <78024299+destroyme123@users.noreply.github.com>
2024-10-10 04:48:37 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cat <<EOL > .devcontainer/devcontainer.json
             "version": "latest"
         }
     },
-    "postStartCommand": "python3 /workspaces/choco/raj.py",
+    "postStartCommand": "python3 /workspaces/choco-/raj.py",
     "customizations": {
         "vscode": {
             "settings": {
